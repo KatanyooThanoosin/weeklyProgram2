2c2447ff9dd5e262f2dde97d093675768038d9fd
KatanyooThanoosin <88848316+KatanyooThanoosin@users.noreply.github.com>
2021-08-14 17:32:54 +0700

    Delete Source.cpp

--- Source.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-	int a[3],min=INT_MAX;
-	for (int i = 0; i < 3; i++) cin >> a[i];
-	for (int i = 0; i < 3; i++) if (a[i] < min)min = a[i];
-	cout << min;
-}
